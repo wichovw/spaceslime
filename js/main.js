39fa0901a22f98ed3293f8c2a556e92a722915d9
Wicho Valdeavellano <wichovalde+git@gmail.com>
2015-04-17 18:29:37 -0600

    Some physics added

--- js/main.js
+++ js/main.js
@@ -61,10 +61,80 @@ slime2.rotation.x = -Math.PI/2;
 scene.add( slime1 );
 scene.add( slime2 );
 
+var material = new THREE.MeshPhongMaterial({color: 0xffaa00});
+var geometry = new THREE.SphereGeometry(50, 10, 10);
+var ball = new THREE.Mesh(geometry, material);
+ball.position.z = 600;
+ball.position.y = 800;
+scene.add(ball);
 
 // ----------
 
+// Ball control variables:
+var gravity = -1;
+var friction = -1;
+var slime_r2 = Math.pow(150, 2);
+var ball_r2 = Math.pow(50, 2);
+var collide = slime_r2 + ball_r2;
+var force_factor = 0.2;
+var ball_ = {
+    x: 600,
+    y: 800,
+    x_vel: 0,
+    y_vel: 0
+}
+var slime1_ = {
+    x: 600,
+    y: 0,
+    x_vel: 0,
+    y_vel: 0
+}
+
+function changeBallPosition() {
+    ball_.x += ball_.x_vel;
+    ball_.y += ball_.y_vel;
+    ball_.y_vel += gravity;
+}
+
+function changeSlimesPosition() {
+    slime1_.x += slime1_.x_vel;
+    slime1_.y += slime1_.y_vel;
+    slime1_.y_vel = slime1_.y > 0 ? slime1_.y_vel + gravity : 0;
+//    slime1_.x_vel = slime1_.x_vel > 0 ? slime1_.x_vel + friction : 0;
+}
+
+function detectCollision() {
+    d1 = Math.pow(ball_.x - slime1_.x, 2) + Math.pow(ball_.y - slime1_.y, 2)
+    if (d1 <= collide){
+        ball_.x_vel = (ball_.x - slime1_.x)*force_factor;
+        ball_.y_vel = (ball_.y - slime1_.y + slime1_.y_vel)*force_factor;
+    }
+}
+
+function renderBallAndSlimes() {
+    ball.position.z = ball_.x;
+    ball.position.y = ball_.y;
+    slime2.position.z = slime1_.x;
+    slime2.position.y = slime1_.y;
+}
+
+jQuery(document.documentElement).keyup(function(e){
+    if (e.keyCode == 65){ // A
+        slime1_.x_vel += 10;
+    }
+    else if (e.keyCode == 68){ // D
+        slime1_.x_vel -= 10;
+    }
+    else if (e.keyCode == 87){ // W
+        slime1_.y_vel += 10;
+    }
+});
+
 function toDoEachFrame() {
+    detectCollision();
+    changeBallPosition();
+    changeSlimesPosition();
+    renderBallAndSlimes();
 }
 
 function render() {
